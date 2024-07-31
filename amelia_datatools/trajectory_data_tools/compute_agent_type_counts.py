e847be594a5c21b1df582318d7bc9248b85c7a1e
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-07-31 16:45:25 -0400

    moved directories and created amelia_datatools

--- amelia_datatools/trajectory_data_tools/compute_agent_type_counts.py
+++ amelia_datatools/trajectory_data_tools/compute_agent_type_counts.py
@@ -1,5 +1,6 @@
+from common import AIRPORT_COLORMAP, CACHE_DIR, TRAJ_DATA_DIR, STATS_DIR
 import json
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
@@ -7,20 +8,57 @@ import sys
 
 from tqdm import tqdm
 
-plt.rc('xtick',labelsize=16)
-plt.rc('ytick',labelsize=16)
+plt.rc('xtick', labelsize=16)
+plt.rc('ytick', labelsize=16)
 
 sys.path.insert(1, '../utils/')
-from common import *
 
 cache_dir = os.path.join(CACHE_DIR, __file__.split('/')[-1].split(".")[0])
-os.makedirs(cache_dir, exist_ok=True) 
+os.makedirs(cache_dir, exist_ok=True)
 
 stats_dir = os.path.join(STATS_DIR, __file__.split('/')[-1].split(".")[0])
-os.makedirs(stats_dir, exist_ok=True) 
+os.makedirs(stats_dir, exist_ok=True)
 
 airport_list = AIRPORT_COLORMAP.keys()
 name = {'total': 'Total', '0': 'Aircraft', '1': 'Vehicle', '2': 'Unknown'}
+
+
+def get_agent_counts(file_list: list) -> dict:
+    print(f"\tGetting agent counts...", end="\r")
+    agent_counts = {
+        'total': [], '0': [], '1': [], '2': []
+    }
+    for f in tqdm(file_list):
+        data = pd.read_csv(f)
+
+        unique_frames = np.unique(data.Frame)
+        for frame in unique_frames:
+            agents = data[data.Frame == frame]
+
+            agent_counts['total'].append(agents.shape[0])
+            agent_counts['0'].append(np.where(agents.Type == AgentType.AIRCRAFT.value)[0].shape[0])
+            agent_counts['1'].append(np.where(agents.Type == AgentType.VEHICLE.value)[0].shape[0])
+            agent_counts['2'].append(np.where(agents.Type == AgentType.UNKNOWN.value)[0].shape[0])
+    print(f"\tGetting agent counts...done.",)
+    return agent_counts
+
+
+def compute_agent_count_stats(agent_counts: dict) -> dict:
+    print(f"\tComputing agent counts statistics...", end='\r')
+    agent_count_stats = {}
+    for k, v in agent_counts.items():
+        v = np.asarray(v)
+        agent_count_stats[k] = {
+            "min": round(v.min().astype(float), 5),
+            "max": round(v.max().astype(float), 5),
+            "mean": round(v.mean().astype(float), 5),
+            "std": round(v.std().astype(float), 5)
+        }
+
+        agent_counts[k] = v
+    print(f"\tComputing agent counts statistics...done.")
+    return agent_count_stats, agent_counts
+
 
 def get_agent_type_counts(file_list: list) -> dict:
     print(f"\tGetting agent type counts...", end="\r")
@@ -39,19 +77,21 @@ def get_agent_type_counts(file_list: list) -> dict:
                 agent_types[key] = 0
                 key_p = f"{key}_perc"
                 agent_types[key_p] = 0.0
-                
+
             agent_types[key] += 1
             total_count += 1
 
     for k, v in agent_types.items():
-        if "perc" in k: continue
+        if "perc" in k:
+            continue
         kp = f"{k}_perc"
         agent_types[kp] = round(v / total_count, 3)
 
     agent_types['total'] = total_count
-    
+
     print(f"\tGetting agent type counts...done.",)
     return agent_types
+
 
 if __name__ == "__main__":
     for airport in airport_list:
--- amelia_datatools/utils/common.py
+++ amelia_datatools/utils/common.py
@@ -1,28 +1,36 @@
 from enum import Enum
+import os
 from easydict import EasyDict
 
 # Base paths
-OUT_DIR = "../out"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.join(ROOT_DIR, "../..")
+ROOT_DIR = os.path.normpath(ROOT_DIR)
+OUT_DIR = os.path.join(ROOT_DIR, "output")
+
 VIS_DIR = f"{OUT_DIR}/vis"
 STATS_DIR = f"{OUT_DIR}/stats"
 CACHE_DIR = f"{OUT_DIR}/cache"
 
-DATA_DIR = "../datasets/amelia"
+DATA_DIR = os.path.join(ROOT_DIR, "datasets/amelia")
 VERSION = "a10v08"
 TRAJ_DATA_DIR = f"{DATA_DIR}/traj_data_{VERSION}"
 ASSET_DATA_DIR = f"{DATA_DIR}/assets"
 GRAPH_DATA_DIR = f"{DATA_DIR}/graph_data"
 
 # Global variables
+
+
 class AgentType(Enum):
     AIRCRAFT = 0
-    VEHICLE  = 1
-    UNKNOWN  = 2
+    VEHICLE = 1
+    UNKNOWN = 2
+
 
 AIRPORT_COLORMAP = {
     "panc": "crimson",
     "kbos": "lightcoral",
-    "kdca": "orangered", 
+    "kdca": "orangered",
     "kewr": "#2E8B57",
     "kjfk": "limegreen",
     "klax": "darkturquoise",
@@ -32,7 +40,7 @@ AIRPORT_COLORMAP = {
     "ksfo": "deeppink"
 }
 
-# 
+#
 AIRPORT_CROWDEDNESS = {
     "high": ["klax", "kjfk"],
     "mid": ["ksea", "kewr", "ksfo", "kbos", "kdca", "kmdw"],
@@ -43,31 +51,31 @@ MOTION_PROFILE = {
     'acceleration': {
         'label': 'Acceleration',
         'unit': 'm/s²'
-    }, 
+    },
     'speed': {
         'label': 'Speed',
         'unit': 'm/s'
-    }, 
+    },
     'heading': {
         'label': 'Heading Change',
-        'unit': 'Degrees'   
+        'unit': 'Degrees'
     }
 }
 
 RAW_IDX = EasyDict({
-    'Frame':    0, 
-    'ID':       1, 
-    'Altitude': 2, 
-    'Speed':    3, 
-    'Heading':  4, 
-    'Lat':      5, 
-    'Lon':      6, 
-    'Range':    7, 
-    'Bearing':  8, 
-    'Type':     9, 
-    'Interp':  10, 
-    'x':       11, 
-    'y':       12 
+    'Frame':    0,
+    'ID':       1,
+    'Altitude': 2,
+    'Speed':    3,
+    'Heading':  4,
+    'Lat':      5,
+    'Lon':      6,
+    'Range':    7,
+    'Bearing':  8,
+    'Type':     9,
+    'Interp':  10,
+    'x':       11,
+    'y':       12
 })
 
 KNOTS_2_MS = 1/1.94384
