c2186c030424316a22ce702f4029b3796e8663a3
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-08-02 11:32:21 -0400

    dataset updated

--- amelia_datatools/dataset/debug_interp_csv.py
+++ amelia_datatools/dataset/debug_interp_csv.py
@@ -1,23 +1,21 @@
-import glob
+import scenario_identification.scene_utils.common as C
+from matplotlib.offsetbox import AnnotationBbox
+from tqdm import tqdm
+from itertools import groupby
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import pickle
 import pandas as pd
-import random 
+import random
 random.seed(4242)
 np.set_printoptions(suppress=True)
-from itertools import groupby
 
-from tqdm import tqdm 
-
-from matplotlib.offsetbox import AnnotationBbox
-import scenario_identification.scene_utils.common as C
 
 SUBDIR = __file__.split('/')[-1].split('.')[0]
 INT = '[INT]'
 
-def plot_scene(scenario, assets, filetag, order_list = None, version = 'v5'):
+
+def plot_scene(scenario, assets, filetag, order_list=None, version='v5'):
     raster_map, hold_lines, graph_map, ll_extent, agents = assets
     north, east, south, west = ll_extent
 
@@ -25,12 +23,12 @@ def plot_scene(scenario, assets, filetag, order_list = None, version = 'v5'):
     fig, movement_plot = plt.subplots()
     # Display global map
     movement_plot.imshow(
-        raster_map, zorder=0, extent=[west, east, south, north], alpha=0.8, cmap='gray_r') 
-    
+        raster_map, zorder=0, extent=[west, east, south, north], alpha=0.8, cmap='gray_r')
+
     sequences = scenario['sequences']
     agent_types = scenario['agent_types']
 
-    N, T, D = sequences.shape 
+    N, T, D = sequences.shape
     for n in range(N):
         # Get heading at last point of trajectory
         heading = sequences[n, -1, C.SEQ_IDX['Heading']]
@@ -41,27 +39,26 @@ def plot_scene(scenario, assets, filetag, order_list = None, version = 'v5'):
         lon = sequences[n, :, C.SEQ_IDX['Lon']]
 
         img = C.plot_agent(agents[agent_type], heading, C.ZOOM[agent_type])
-        ab = AnnotationBbox(img, (lon[-1], lat[-1]), frameon=False) 
+        ab = AnnotationBbox(img, (lon[-1], lat[-1]), frameon=False)
         movement_plot.add_artist(ab)
-        movement_plot.plot(lon, lat, color='blue', lw=0.65) 
+        movement_plot.plot(lon, lat, color='blue', lw=0.65)
 
         if version == 'v5':
-            movement_plot.scatter(lon, lat, color='red', lw=0.65, s=2) 
+            movement_plot.scatter(lon, lat, color='red', lw=0.65, s=2)
 
         else:
             interp = sequences[n, :, C.SEQ_IDX['Interp']]
 
             # Place plane on last point of ground truth sequence
             if order_list is None:
-               
-                idx = np.where(interp == 0.0)[0]
-                movement_plot.scatter(lon[idx], lat[idx], color='red', lw=0.65, s=2) 
-                idx = np.where(interp == 1.0)[0]
-                movement_plot.scatter(lon[idx], lat[idx], color='orange', lw=0.65, s=3) 
-                idx = np.where(interp == 2.0)[0]
-                movement_plot.scatter(lon[idx], lat[idx], color='yellow', lw=0.65, s=5) 
 
-    
+                idx = np.where(interp == 0.0)[0]
+                movement_plot.scatter(lon[idx], lat[idx], color='red', lw=0.65, s=2)
+                idx = np.where(interp == 1.0)[0]
+                movement_plot.scatter(lon[idx], lat[idx], color='orange', lw=0.65, s=3)
+                idx = np.where(interp == 2.0)[0]
+                movement_plot.scatter(lon[idx], lat[idx], color='yellow', lw=0.65, s=5)
+
     # Get conflict points (Hold lines) and plot them on the map
     # hold_lines = pickle_map[pickle_map[:, MAP_IDX['SemanticID']] == 1]
     hold_lines_lon = hold_lines[:, C.MAP_IDX['LonStart']]
@@ -70,8 +67,9 @@ def plot_scene(scenario, assets, filetag, order_list = None, version = 'v5'):
 
     C.save(filetag)
 
+
 if __name__ == "__main__":
-    import argparse 
+    import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--airport', default="ksea", choices=["ksea", "kewr"])
     parser.add_argument("--base_dir", type=str, default="../../datasets/amelia_v6/raw_trajectories")
@@ -81,7 +79,7 @@ if __name__ == "__main__":
     out_dir = os.path.join(args.out_dir, SUBDIR, args.airport)
     os.makedirs(out_dir, exist_ok=True)
 
-    map_dir = os.path.join(f"../../datasets/amelia_v5/maps/{args.airport}")    
+    map_dir = os.path.join(f"../../datasets/amelia_v5/maps/{args.airport}")
     assets = C.load_assets(map_dir=map_dir)
 
     version = args.base_dir.split('/')[3].split('_')[-1]
@@ -101,27 +99,27 @@ if __name__ == "__main__":
         # get file timestamp
         timestamp = csv_file.split('/')[-1].split('.')[0].split('_')[-1]
         data = pd.read_csv(csv_file)
-        
+
         agent_IDs = data.ID.unique().tolist()
         total_agents += len(agent_IDs)
 
         for agent_ID in agent_IDs:
-            agent_seq = data[:][data.ID == agent_ID] 
+            agent_seq = data[:][data.ID == agent_ID]
             agent_interp = agent_seq.Interp.to_numpy()
             if INT in agent_interp:
                 x = (agent_interp == INT).astype(int).tolist()
-                grouped = (list(g) for _,g in groupby(enumerate(x), lambda t:t[1]))
+                grouped = (list(g) for _, g in groupby(enumerate(x), lambda t: t[1]))
                 for g in grouped:
                     if g[0][1] == 1:
                         t = 1 if len(g) == 1 else g[-1][0] - g[0][0]
                         interp_T.append(t)
                 interp_agents += 1
-        
+
         perc_interp = round(100 * interp_agents / total_agents, 4)
         perc_files = round(100 * (n+1)/N, 4)
         print(f"Interp. agents: {perc_interp}% Interp. files: {perc_files}%", end="\r")
 
-    perc_out = f"Percentage of interpolated agents: {perc_interp}%\n"    
+    perc_out = f"Percentage of interpolated agents: {perc_interp}%\n"
     str_out = f"Total Files: {N}, Total Trajectories: {total_agents} Interp Traj: {interp_agents}\n"
     print(str_out)
     with open(f"{out_dir}/stats.txt", "w") as f:
