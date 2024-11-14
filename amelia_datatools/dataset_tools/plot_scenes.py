284dc766594158e2ef6188426f9482da5ceb29d6
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-11-13 19:54:03 -0500

    updated: plos_scene for current dataset

--- amelia_datatools/dataset_tools/plot_scenes.py
+++ amelia_datatools/dataset_tools/plot_scenes.py
@@ -2,24 +2,31 @@ import os
 from tqdm import tqdm
 import pickle
 import amelia_datatools.utils.common as C
-from amelia_datatools.utils.utils import get_scene_list, plot_scene, get_file_name
-from amelia_scenes.splitting.dataset import load_assets
+from amelia_datatools.utils import utils as U
+from amelia_scenes.utils.dataset import load_assets
+# from amelia_scenes.visualization.scenes_viz import plot_scene_simple
+
+
+def plot_scenes():
+    pass
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
+    airports = U.get_airport_list()
     parser = ArgumentParser()
-    parser.add_argument('--airport', default="kbos", type=str, help='ICAO Airport Code')
+    parser.add_argument('--airport', default="katl", type=str,
+                        help='ICAO Airport Code', choices=airports)
     parser.add_argument('--base_dir', default=C.DATA_DIR, type=str, help='Input path')
     parser.add_argument('--traj_version', type=str, default=C.VERSION)
     parser.add_argument(
-        '--output_dir', default=f"{C.OUTPUT_DIR}/{get_file_name(__file__)}", type=str)
+        '--output_dir', default=f"{C.OUTPUT_DIR}/{U.get_file_name(__file__)}", type=str)
     parser.add_argument('--max_scenes', default=100, type=int)
     args = parser.parse_args()
 
     assets = load_assets(input_dir=args.base_dir, airport=args.airport)
-    scenes, trajdirs = get_scene_list(airpoirt=args.airport, base_dir=args.base_dir,
-                                      version=args.traj_version)
+    scenes, trajdirs = U.get_scene_list(airport=args.airport, base_dir=args.base_dir,
+                                        version=args.traj_version)
 
     if len(scenes) > args.max_scenes:
         scenes = scenes[:args.max_scenes]
@@ -31,6 +38,6 @@ if __name__ == '__main__':
 
         subdir = os.path.join(args.output_dir, args.airport, trajdir.split('/')[-1])
         scenario_id = scene_data['scenario_id']
-        filetag = os.path.join(args.output_dir, f"{scenario_id}")
+        filetag = os.path.join(subdir, f"{scenario_id}")
         os.makedirs(subdir, exist_ok=True)
-        plot_scene(scenario=scene_data, assets=assets, filetag=filetag)
+        U.plot_scene(scenario=scene_data, assets=assets, filetag=filetag)
